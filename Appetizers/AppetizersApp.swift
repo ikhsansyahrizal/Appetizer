7aa1067fa9c0dc34782dd84388f34c0b72e8271c
Ikhsan Syahrizal <ikhsan.syahrizal@spesolution.com>
2024-03-28 11:03:46 +0700

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Ikhsan on 28/03/24.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
